df213f78e97817bb8ce411d0d826d72d11030169
adriansarmiento-code <ransar39@gmail.com>
2024-10-26 15:24:39 +0800

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+/* Sarmiento, Adrian P.
+   WD-201
+   October 26, 2024 
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.nav-menu li a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
+});
+
